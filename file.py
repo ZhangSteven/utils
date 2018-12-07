962dac41353cfdd22741528e2c6ca95a217f0e3b
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2018-12-07 14:45:15 +0800

    file.py added

---
+++ file.py
@@ -0,0 +1,31 @@
+﻿# coding=utf-8
+# 
+from os import listdir
+from os.path import isfile, isdir, join
+
+
+
+def getFiles(directory):
+	"""
+	[String] directory => [List] file names under that directory
+
+	sub folders are not included
+	"""
+	return [f for f in listdir(directory) if isfile(join(directory, f))]
+
+
+
+def getSubFolders(directory):
+	"""
+	[String] directory => [List] sub folder names under that directory
+
+	files are not included
+	"""
+	return [f for f in listdir(directory) if isdir(join(directory, f))]
+
+
+
+
+if __name__ == '__main__':
+	print(getFiles('.'))		# show local directory files
+	print(getSubFolders('.'))	# show local sub directories
--- utility.py
+++ utility.py
@@ -1,19 +1,7 @@
 # coding=utf-8
 # 
 
-import os, csv
-
-
-
-def get_current_path():
-	"""
-	Get the absolute path to the directory where this module is in.
-
-	This piece of code comes from:
-
-	http://stackoverflow.com/questions/3430372/how-to-get-full-path-of-current-files-directory-in-python
-	"""
-	return os.path.dirname(os.path.abspath(__file__))
+import csv
 
 
 
